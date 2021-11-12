4ebd3ddac724fdd73bfb1708ebd9f66e0602633b
Endreffy Zsolt <ifjezsolt@gmail.com>
2021-11-13 00:22:33 +0100

    Initial commit with working slider and chart

---
+++ src/components/SinChart.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="small">
+    <div class="chart">
+      <line-chart :chart-data="datacollection" :options="options"></line-chart>
+    </div>
+    <vue-slider
+      @change="updateData()"
+      v-model="frequency"
+      :min="1"
+      :max="10"
+      :interval="1"
+      :marks="true"
+    ></vue-slider>
+  </div>
+</template>
+
+<script>
+import LineChart from './LineChart.js'
+import VueSlider from 'vue-slider-component'
+import 'vue-slider-component/theme/default.css'
+
+export default {
+  components: {
+    LineChart,
+    VueSlider,
+  },
+  data () {
+    return {
+      datacollection: {},
+      options: {
+        responsive: true,
+        legend: {
+          display: false,
+        },
+        scales: {
+          xAxes: [{
+            display: false,
+          }],
+          yAxes: [{
+            display: false,
+          }],
+        },
+      },
+      pointCount: 1000,
+      frequency: 1,
+    }
+  },
+  mounted () {
+    this.updateData()
+  },
+  methods: {
+    updateData () {
+      this.datacollection = {
+        labels: this.getPoints(this.pointCount),
+        datasets: [
+          {
+            label: 'Data One',
+            borderColor: '#f87979',
+            backgroundColor: 'rgba(0,0,0,0)',
+            data: this.getSin(this.getPoints(this.pointCount), this.frequency),
+            pointRadius: 1,
+          }
+        ]
+      }
+    },
+    getSin(points, freq) {
+      let d = [];
+      for (let point of points) {
+        d.push(Math.sin(freq * point));
+      }
+      return d;
+    },
+    getPoints(count) {
+      let a = [];
+      for (let i=0;i<count;i++) {
+        a.push(Math.PI * 2 / count * i);
+      }
+      a.push(Math.PI *2 );
+      console.log(a.length);
+      return a;
+    }
+  }
+}
+</script>
+
+<style>
+  .small {
+    max-width: 600px;
+    margin:  150px auto;
+  }
+
+  .chart {
+    border: 1px solid black;
+  }
+</style>

331669e72ffe554c9bfde575b05e67ca5e578d98
Zsolt Endreffy <zsolt.endreffy@commsignia.com>
2021-11-13 15:53:10 +0100

    added random wave and code refactor

--- src/components/SinChart.vue
+++ src/components/SinChart.vue
@@ -3,16 +3,19 @@
     <div class="chart">
       <line-chart :chart-data="datacollection" :options="options"></line-chart>
     </div>
-    <vue-slider
-      @change="updateData()"
-      v-model="frequency"
-      :min="0"
-      :max="25"
-      :interval="1"
-      :marks="false"
-      :tooltip-placement="'bottom'"
-      :tooltip-formatter="formatter"
-    ></vue-slider>
+    <div class="slider-holder">
+      <vue-slider
+          @change="updateChart()"
+          v-model="frequency"
+          :min="0"
+          :max="25"
+          :interval="1"
+          :marks="false"
+          :tooltip-placement="'bottom'"
+          :tooltip-formatter="formatter"
+      ></vue-slider>
+    </div>
+    <md-button class="md-raised md-primary" @click="getNewRandom()">Get new random chart</md-button>
   </div>
 </template>
 
@@ -26,11 +29,12 @@ export default {
     LineChart,
     VueSlider,
   },
-  data () {
+  data() {
     return {
       datacollection: {},
       options: {
         responsive: true,
+        maintainAspectRatio: false,
         legend: {
           display: false,
         },
@@ -42,44 +46,69 @@ export default {
             display: false,
           }],
         },
+        tooltips: {
+          enabled: false
+        }
       },
-      pointCount: 1000,
+      points: this.getPoints(1000),
       frequency: 0,
-      formatter: '{value} Hz',
+      formatter: v => v * 4 + ' %',
+      randomWave: {}
     }
   },
-  mounted () {
-    this.updateData()
+  mounted() {
+    this.randomWave = {
+      label: 'Random wave',
+      borderColor: 'rgba(0,100,200, 1)',
+      backgroundColor: 'rgba(0,0,0,0)',
+      data: this.getSin(this.points, Math.floor(Math.random() * 26)),
+      pointRadius: 0,
+      borderWidth: 7,
+    };
+    this.updateChart();
   },
   methods: {
-    updateData () {
-      this.datacollection = {
-        labels: this.getPoints(this.pointCount),
-        datasets: [
+    updateChart() {
+      this.datacollection =
           {
-            label: 'Data One',
-            borderColor: '#f87979',
-            backgroundColor: 'rgba(0,0,0,0)',
-            data: this.getSin(this.getPoints(this.pointCount), this.frequency),
-            pointRadius: 1,
+            labels: this.points,
+            datasets: [
+              {
+                label: 'Guessed wave',
+                borderColor: '#f87979',
+                backgroundColor: 'rgba(0,0,0,0)',
+                data: this.getSin(this.points, this.frequency),
+                pointRadius: 0,
+                borderWidth: 3,
+              },
+              this.randomWave
+            ]
           }
-        ]
-      }
+    },
+    getNewRandom() {
+      this.randomWave = {
+        label: 'Random wave',
+        borderColor: 'rgba(0,100,200, 1)',
+        backgroundColor: 'rgba(0,0,0,0)',
+        data: this.getSin(this.points, Math.floor(Math.random() * 26)),
+        pointRadius: 0,
+        borderWidth: 7,
+      };
+      this.updateChart();
     },
     getSin(points, freq) {
       let d = [];
       for (let point of points) {
-        d.push(Math.sin(freq * point));
+        d.push(Math.sin(freq * point / 2));
       }
       return d;
     },
     getPoints(count) {
       let a = [];
-      for (let i=0;i<count;i++) {
+      for (let i = 0; i < count; i++) {
         a.push(Math.PI * 2 / count * i);
       }
-      a.push(Math.PI *2 );
-      console.log(a.length);
+      a.push(Math.PI * 2);
       return a;
     }
   }
@@ -87,12 +116,20 @@ export default {
 </script>
 
 <style>
-  .small {
-    max-width: 600px;
-    margin:  150px auto;
-  }
+.small {
+  position: relative;
+  width: 40vw;
+  margin: auto;
+}
 
-  .chart {
-    border: 1px solid black;
-  }
+.chart {
+  border: 1px solid black;
+  position: relative;
+  width: 40vw;
+  margin: auto;
+}
+
+.slider-holder {
+  margin: 30px 0;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
+import { MdButton } from 'vue-material/dist/components'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+Vue.use(MdButton)
 
 new Vue({
   render: h => h(App),
